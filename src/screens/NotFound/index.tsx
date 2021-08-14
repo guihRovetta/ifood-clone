5929864c49bfce5702fb2fb069546a3e073628d3
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-14 16:08:06 -0300

    feat: create initial not found component and style

---
+++ src/screens/NotFound/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+const NotFound = () => {
+  return (
+    <View style={styles?.container}>
+      <Text>Conteúdo em desenvolvimento!</Text>
+    </View>
+  );
+};
+
+export default NotFound;
