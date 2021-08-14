f6bca6eb631f3752e55ec889028cf23ec66dfd60
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-14 16:07:00 -0300

    style: create global colors and icons files

---
+++ src/global/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  white: '#FFFFFF',
+
+  lightGray: '#717171',
+  mediumGray: '3f3e3e',
+  darkGray: '#3e3e3e',
+};
---
+++ src/global/styles/icons.ts
@@ -0,0 +1,23 @@
+import ArrowDownIcon from '../../assets/icons/arrow-down.svg';
+import HomeFilledIcon from '../../assets/icons/home-filled.svg';
+import HomeOutlinedIcon from '../../assets/icons/home-outlined.svg';
+import OrdersFilledIcon from '../../assets/icons/orders-filled.svg';
+import OrdersOutlinedIcon from '../../assets/icons/orders-outlined.svg';
+import ProfileFilledIcon from '../../assets/icons/profile-filled.svg';
+import ProfileOutlinedIcon from '../../assets/icons/profile-outlined.svg';
+import SearchFilledIcon from '../../assets/icons/search-filled.svg';
+import SearchOutlinedIcon from '../../assets/icons/search-outlined.svg';
+import SuperRestaurantIcon from '../../assets/icons/super-restaurant.svg';
+
+export {
+  ArrowDownIcon,
+  HomeFilledIcon,
+  HomeOutlinedIcon,
+  OrdersFilledIcon,
+  OrdersOutlinedIcon,
+  ProfileFilledIcon,
+  ProfileOutlinedIcon,
+  SearchFilledIcon,
+  SearchOutlinedIcon,
+  SuperRestaurantIcon,
+};
