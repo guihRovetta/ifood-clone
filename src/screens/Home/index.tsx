eb3825ba14bc62302b2bb1093f6eb866c38b67b9
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-20 00:31:36 -0300

    feat: add home filters and switch case home components to home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Text, View } from 'react-native';
 import Animated from 'react-native-reanimated';
 
 import Header from '../../components/Header';
+import HomeFilters from '../../components/HomeFilters';
+import SwitchCaseHome from '../../components/SwitchCaseHome';
+import { categoryList } from '../../data/categoryList';
 import { styles } from './styles';
 
 const HEADER_HEIGHT = 48 * 2;
 const data = [...Array(70).keys()];
 
 const Home = () => {
+  const [category, setCategory] = useState(categoryList[0]?.key);
+
   const scrollY = useRef(new Animated.Value(0));
   const scrollYClamped = Animated.diffClamp(scrollY.current, 0, HEADER_HEIGHT);
 
@@ -31,30 +36,40 @@ const Home = () => {
     }
   );
 
+  const handleChangeCategory = (newCategory: string) => {
+    setCategory(newCategory);
+  };
+
   return (
     <View style={styles.container}>
       <Animated.View style={[styles.header, { transform: [{ translateY }] }]}>
         <StatusBar style="dark" />
-        <Header headerHeight={HEADER_HEIGHT} />
+        <Header
+          headerHeight={HEADER_HEIGHT}
+          categoryList={categoryList}
+          category={category}
+          handleChangeCategory={handleChangeCategory}
+        />
       </Animated.View>
 
-      <Animated.Text
+      <Animated.View
         style={[
           styles.header,
           { marginTop: HEADER_HEIGHT, transform: [{ translateY }] },
         ]}
       >
-        Componente de Filtros
-      </Animated.Text>
+        <HomeFilters />
+      </Animated.View>
 
       <Animated.ScrollView
         bounces={false}
         scrollEventThrottle={16}
         contentContainerStyle={{
-          marginTop: HEADER_HEIGHT,
+          marginTop: HEADER_HEIGHT + 48,
         }}
         onScroll={handleScroll}
       >
+        <SwitchCaseHome category={category} categoryList={categoryList} />
         {data?.map((item, index) => (
           <Text key={index}>{item}</Text>
         ))}
