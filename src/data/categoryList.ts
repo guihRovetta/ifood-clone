bd737be22dc665566e0d603fbb252f07600bcbe9
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-20 00:28:04 -0300

    feat: create fake category list data and function to generate items

---
+++ src/data/categoryList.ts
@@ -0,0 +1,9 @@
+import { generateItemList } from '../utils/generateItemList';
+
+export const categoryList = [
+  generateItemList('restaurants', 'Restaurantes'),
+  generateItemList('market', 'Mercados'),
+  generateItemList('drinks', 'Bebidas'),
+  generateItemList('drugstore', 'Farmácias'),
+  generateItemList('pets', 'Pets'),
+];
---
+++ src/utils/generateItemList.ts
@@ -0,0 +1,3 @@
+export const generateItemList = (key: string, label: string) => {
+  return { key, label };
+};
