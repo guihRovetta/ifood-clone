df68eac5712d522671244105a4baf1d0dbd708ac
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-19 22:24:44 -0300

    style: create global font style

---
+++ src/global/styles/fonts.ts
@@ -0,0 +1,17 @@
+export const fonts = {
+  font: {
+    bold: 'sulsans-bold',
+    boldItalic: 'bold-italic',
+    light: 'sulsans-light',
+    medium: 'sulsans-medium',
+    regular: 'sulsans-regular',
+  },
+
+  size: {
+    xsmall: 10,
+    small: 12,
+    medium: 14,
+    large: 16,
+    xlarge: 20,
+  },
+};
