d7681c2f53ed96a8aa450a8a106938305dd7d407
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-22 23:06:20 -0300

    feat: import banners list component to restaurants component

--- src/components/Restaurants/index.tsx
+++ src/components/Restaurants/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { categoryImageList } from '../../data/categoryImageList';
+import { restaurantsBannersList } from '../../data/restaurantsBannersList';
+import BannersList from '../BannersList';
 import CategoryImageList from '../CategoryImageList';
 
 const Restaurants = () => {
   return (
     <View>
       <CategoryImageList items={categoryImageList} />
+
+      <BannersList items={restaurantsBannersList} />
     </View>
   );
 };
