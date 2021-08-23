58de92cae6d92f328e2de4700632734d1fb279dc
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-22 23:03:43 -0300

    feat: create restaurants banners list array data

---
+++ src/data/restaurantsBannersList.ts
@@ -0,0 +1,15 @@
+import Banner1 from '../assets/banners/1.png';
+import Banner2 from '../assets/banners/2.png';
+import Banner3 from '../assets/banners/3.png';
+import Banner4 from '../assets/banners/4.png';
+import Banner5 from '../assets/banners/5.png';
+import Banner6 from '../assets/banners/6.png';
+
+export const restaurantsBannersList = [
+  Banner1,
+  Banner2,
+  Banner3,
+  Banner4,
+  Banner5,
+  Banner6,
+];
