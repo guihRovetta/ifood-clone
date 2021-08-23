4550e1bdc24fe57ca3e75ba2be8eeffc0e6abc3c
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-22 23:03:19 -0300

    feat: import filter and pickup icons to global icons

--- src/global/styles/icons.ts
+++ src/global/styles/icons.ts
@@ -1,8 +1,10 @@
 import ArrowDownIcon from '../../assets/icons/arrow-down.svg';
+import FilterIcon from '../../assets/icons/filter.svg';
 import HomeFilledIcon from '../../assets/icons/home-filled.svg';
 import HomeOutlinedIcon from '../../assets/icons/home-outlined.svg';
 import OrdersFilledIcon from '../../assets/icons/orders-filled.svg';
 import OrdersOutlinedIcon from '../../assets/icons/orders-outlined.svg';
+import PickupIcon from '../../assets/icons/pickup.svg';
 import ProfileFilledIcon from '../../assets/icons/profile-filled.svg';
 import ProfileOutlinedIcon from '../../assets/icons/profile-outlined.svg';
 import QrCodeIcon from '../../assets/icons/qr-code.svg';
@@ -22,4 +24,6 @@ export {
   SearchFilledIcon,
   SearchOutlinedIcon,
   SuperRestaurantIcon,
+  FilterIcon,
+  PickupIcon,
 };
