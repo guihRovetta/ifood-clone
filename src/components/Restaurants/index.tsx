4c6b68f6c2c3f7803c7ce99abc6c6445d34b50bd
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-24 23:20:09 -0300

    feat: add store list components and ultra wide banner components to restaurants

--- src/components/Restaurants/index.tsx
+++ src/components/Restaurants/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Banner01 from '../../assets/ultrawide-banners/1.jpeg';
+import Banner02 from '../../assets/ultrawide-banners/2.jpeg';
 import { categoryImageList } from '../../data/categoryImageList';
+import { favoritesStoresList } from '../../data/favoritesStoresList';
+import { ifoodFamousStoresList } from '../../data/ifoodFamousStoresList';
+import { lastStoresList } from '../../data/lastStoresList';
 import { restaurantsBannersList } from '../../data/restaurantsBannersList';
 import BannersList from '../BannersList';
 import CategoryImageList from '../CategoryImageList';
+import StoreList from '../StoreList';
+import UltraWideBanner from '../UltraWideBanner';
+import { styles } from './styles';
 
 const Restaurants = () => {
   return (
@@ -12,6 +20,34 @@ const Restaurants = () => {
       <CategoryImageList items={categoryImageList} />
 
       <BannersList items={restaurantsBannersList} />
+
+      <StoreList
+        title="Últimas lojas"
+        showMoreButton
+        items={lastStoresList}
+        style={styles.genericList}
+      />
+
+      <StoreList
+        title="Meus Favoritos"
+        showMoreButton
+        items={favoritesStoresList}
+        style={styles.genericList}
+      />
+
+      <View style={styles.genericBanner}>
+        <UltraWideBanner source={Banner01} />
+      </View>
+
+      <StoreList
+        title="Famosos no iFood"
+        items={ifoodFamousStoresList}
+        style={styles.genericList}
+      />
+
+      <View style={styles.genericBanner}>
+        <UltraWideBanner source={Banner02} />
+      </View>
     </View>
   );
 };
