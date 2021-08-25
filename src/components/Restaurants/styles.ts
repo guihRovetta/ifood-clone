2baa1dbb68c96df1f0359d6b895c579857150b55
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-08-24 23:19:30 -0300

    style: create styles to restaurants component

--- src/components/Restaurants/styles.ts
+++ src/components/Restaurants/styles.ts
@@ -1,3 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  genericList: {
+    paddingHorizontal: 12,
+    marginTop: 32,
+  },
+  genericBanner: {
+    marginTop: 32,
+  },
+});
