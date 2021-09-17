aef92fb9ee2f969d4701452e3410d652c08b2e04
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-09-16 21:51:49 -0300

    feat: add function parameter to check if is minutes or hours

--- src/utils/generateMinAndMaxTime.ts
+++ src/utils/generateMinAndMaxTime.ts
@@ -1,6 +1,10 @@
 import { generateRamdomNumber } from './generateRandomNumber';
 
-export const generateMinAndMaxTime = (min: number, max: number) => {
+export const generateMinAndMaxTime = (
+  min: number,
+  max: number,
+  isMinutes = true
+) => {
   const minTime = generateRamdomNumber(min, max, true);
-  return [minTime, minTime + 10];
+  return [minTime, minTime + (isMinutes ? 10 : 1)];
 };
