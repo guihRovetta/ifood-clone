36b5093b068e5d241e2a383a5858ac8ee29744ee
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-09-16 21:52:20 -0300

    feat: create market banners list array

---
+++ src/data/marketBannersList.ts
@@ -0,0 +1,7 @@
+import Banner1 from '../assets/market/banners/1.png';
+import Banner2 from '../assets/market/banners/2.jpeg';
+import Banner3 from '../assets/market/banners/3.png';
+import Banner4 from '../assets/market/banners/4.png';
+import Banner5 from '../assets/market/banners/5.png';
+
+export const marketBannersList = [Banner1, Banner2, Banner3, Banner4, Banner5];
