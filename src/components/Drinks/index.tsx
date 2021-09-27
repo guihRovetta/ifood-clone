9fcc634a281dcf47837cb5dc6e1a85d5debb176c
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-09-27 11:49:02 -0300

    feat: add product card list and store item list components to drinks component, adjust style

--- src/components/Drinks/index.tsx
+++ src/components/Drinks/index.tsx
@@ -4,12 +4,16 @@ import { View } from 'react-native';
 import Banner01 from '../../assets/drinks/wide-banners/1.jpeg';
 import { drinksBannersList } from '../../data/drinksBannersList';
 import { drinksCategoriesImageList } from '../../data/drinksCategoriesImageList';
+import { drinksMostRequestedProductsList } from '../../data/drinksMostRequestedProductsList';
+import { drinksMostRequestedStoresList } from '../../data/drinksMostRequestedStoresList';
 import BannersList from '../BannersList';
 import CategoryImageList from '../CategoryImageList';
+import ProductCardList from '../ProductCardList';
+import StoreItemList from '../StoreItemList';
 import UltraWideBanner from '../UltraWideBanner';
 import { styles } from './styles';
 
-const Drinks: React.FC = () => {
+const Drinks = () => {
   return (
     <View>
       <CategoryImageList items={drinksCategoriesImageList} />
@@ -18,6 +22,21 @@ const Drinks: React.FC = () => {
 
       <View style={styles.genericBannerWrapper}>
         <UltraWideBanner source={Banner01} />
+      </View>
+
+      <View style={styles.productCardListWrapper}>
+        <ProductCardList
+          title="Bebidas mais pedidas"
+          items={drinksMostRequestedProductsList}
+          showMoreButton
+        />
+      </View>
+
+      <View style={styles.storeItemListWrapper}>
+        <StoreItemList
+          title="Mais pedidos"
+          items={drinksMostRequestedStoresList}
+        />
       </View>
     </View>
   );
--- src/components/Drinks/styles.ts
+++ src/components/Drinks/styles.ts
@@ -4,4 +4,12 @@ export const styles = StyleSheet.create({
   genericBannerWrapper: {
     marginTop: 32,
   },
+  productCardListWrapper: {
+    marginTop: 32,
+    marginLeft: 12,
+  },
+  storeItemListWrapper: {
+    paddingHorizontal: 12,
+    marginTop: 32,
+  },
 });
