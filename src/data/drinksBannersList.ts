a73b4d56bcc254622bc414aa43c0891936d0a6a0
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-09-28 14:59:13 -0300

    feat: add more banners to drinks banners list

--- src/data/drinksBannersList.ts
+++ src/data/drinksBannersList.ts
@@ -1,3 +1,4 @@
 import Banner1 from '../assets/drinks/banners/1.png';
+import Banner2 from '../assets/drinks/banners/2.png';
 
-export const drinksBannersList = [Banner1];
+export const drinksBannersList = [Banner1, Banner2];
